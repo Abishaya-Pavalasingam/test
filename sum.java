6099c24c5800c048898f856739d0098f3d87ceda
Abishaya-Pavalasingam <107987118+Abishaya-Pavalasingam@users.noreply.github.com>
2023-01-03 14:31:05 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -8,6 +8,7 @@ class sum
         int c = 6;
 =======
         int c = 10;
+        int d = 9;
 >>>>>>> d12a4efccba20f8193e05ea06e0ebb6a6c1fca24
         System.out.print("sum of these numbers"+a+b);
     }

9a3c26eca419d13d373635f96d8f4e3fb46159b9
Abishaya-Pavalasingam <107987118+Abishaya-Pavalasingam@users.noreply.github.com>
2023-01-03 14:42:49 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -4,12 +4,9 @@ class sum
     {
         int a = 7;
         int b = 4;
-<<<<<<< HEAD
         int c = 6;
-=======
         int c = 10;
         int d = 9;
->>>>>>> d12a4efccba20f8193e05ea06e0ebb6a6c1fca24
         System.out.print("sum of these numbers"+a+b);
     }
 }
